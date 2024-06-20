472233047aabfb82cfd75871278d1729c3d793ee
David Lopez <davidlopez@MacBook-Pro-3.lan>
2024-06-20 18:04:09 -0500

    new changes

--- tests/specs/Profile.spec.js
+++ tests/specs/Profile.spec.js
@@ -3,48 +3,40 @@ const { POManager } = require('../pageObjects/POManager.js');
 const URL = JSON.parse(JSON.stringify(require("../testData/pageUrl.json")));
 
 test.describe('Profile regsitration, edit and sign in', ()=>{
+    let pm;
 
     test.beforeEach(async ({page})=>{
     await page.goto(URL.url);
     await expect(page).toHaveTitle('Home Page');
+    pm = new POManager(page);
     });
 
     test('Create new customer account', async ({page}) => {
-        const pm = new POManager(page);
-
         await pm.homePage.clickCreateAccount();
         await pm.createAccount.createNewAccount();
         });
 
     test('Create Account from Login page', async ({page})=>{
-        const pm = new POManager(page);
-
         await pm.homePage.clickSignIn();
         await pm.login.createAnAccount();
         await pm.createAccount.createNewAccount();
     });
 
     test ('Validate wrong confirm password', async ({page})=> {
-        const pm = new POManager(page);
-
         await pm.homePage.clickCreateAccount();
         await pm.createAccount.wrongConfirmationPassword();
     })
 
     test('Validate existing email address', async ({page}) => {
-        const pm = new POManager(page);
-
         await pm.homePage.clickCreateAccount();
         await pm.createAccount.existingEmailAccount();
     })
 
     test ('Customer login successfuly', async ({page}) =>{
-        const pm = new POManager(page);
         await pm.login.logIn();
     })
 
     test('Customer log out', async ({page}) => {
-        const pm = new POManager(page);
         await pm.login.logOut();
     })
 
--- tests/specs/ShoppingCart.spec.js
+++ tests/specs/ShoppingCart.spec.js
@@ -3,14 +3,14 @@ const { POManager } = require('../pageObjects/POManager.js');
 const URL = JSON.parse(JSON.stringify(require("../testData/pageUrl.json")));
 
 test.describe('Add products to shopping cart and checkout', ()=>{
+    let pm;
     test.beforeEach(async ({page})=>{
     await page.goto(URL.url);
     await expect(page).toHaveTitle('Home Page');
+    pm = new POManager (page);
     });
 
     test('Add different items to shopping cart', async ({page})=>{
-        const pm = new POManager (page);
-
         await pm.homePage.clickJackets();
         await page.waitForLoadState('networkidle');
         await pm.women.selectColorBlue();
@@ -19,8 +19,5 @@ test.describe('Add products to shopping cart and checkout', ()=>{
         await pm.shoppingCart.clickShoppingCart();
         const expectedProducts = await pm.shoppingCart.validateCartProducts();
         expect (selectedProducts.sort()).toEqual(expectedProducts.sort())
-
     });
-
-
 });
